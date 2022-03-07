a6b4591cb34b23a5215523dc2c4d8d83144892ea
UH5555 <zhuhao162@163.com>
2022-03-07 19:05:50 +0800

    0307\初始指针（二）

--- test.c
+++ test.c
@@ -169,10 +169,65 @@
 //	*--vp = 0;
 //}
 
-int main()
-{
-	int arr[10] = { 0 };
-	printf("%p\n", arr);//地址-首元素的地址
-	printf("%p\n", &arr[0]);
-	return 0;
-}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%p\n", arr);//地址-首元素的地址
+//	printf("%p\n", arr+1);
+//
+//	printf("%p\n", &arr[0]);
+//	printf("%p\n", &arr[0]+1);
+//
+//	printf("%p\n", &arr);
+//	printf("%p\n", &arr+1);
+//	//1.%arr - &数组名不是首元素的地址 - 数组名表示真个数组 - &数组名  取出的是整个数组的地址
+//	//2.sizeof(arr) - sizeof(数组名) - 数组名表示的整个数组 - sizeof(数组名)计算的是整个数组的大小
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%p ==== %p\n", p + i, &arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	int** ppa = &pa;//ppa就是二级指针
+//	**ppa = 20;
+//	printf("%d\n", **ppa);
+//	return 0;
+//}
+
+////指针数组 - 数组 - 存放指针的数组
+////数组指针 - 指针
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int* arr[3] = { &a,&b,&c };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%d ", *(arr[i]));
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	{
+//		int a = 10;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
